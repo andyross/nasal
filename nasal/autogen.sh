a5a3d82d43663189835aa3890760f22b91221117
Andy Ross <andy@plausible.org>
2006-07-06 21:01:32 +0000

    Update distrubution stuff

--- nasal/autogen.sh
+++ nasal/autogen.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-aclocal && autoconf && automake -a
+#!/bin/bash
+(cd src; ln -s ../../*.[ch])
+touch config.h.in
+aclocal
+autoconf
+automake -a
+./configure
+make dist
+make distclean
+
